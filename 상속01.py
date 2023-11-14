b727e38205f8a2e6340eb2bf8784ca5a9498a690
ilim0507 <ilim0507@gmail.com>
2023-11-14 16:49:32 +0900

    End of Day 2

--- 상속01.py
+++ 상속01.py
@@ -6,17 +6,31 @@ class Person:
     def printInfo(self):
         print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
 
+    def working(self):
+        print("Currently working...")
+
+    def coding(self):
+        print("Currently coding...")
+
 class Student(Person):
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        Person.__init__(self, name, phoneNumber) # replaces bottom two lines
+        #self.name = name
+        #self.phoneNumber = phoneNumber
         self.subject = subject
         self.studentID = studentID
+        
+    def printInfo(self): # overwrites parent method
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(Major :{0}, Sutdent ID : {1})".format(self.subject, self.studentID))
 
 
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
 print(p.__dict__)
 print(s.__dict__)
-
+p.printInfo()
+s.printInfo()
+s.working()
+s.coding()
 
