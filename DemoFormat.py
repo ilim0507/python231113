4d0ffaca49c2ee1a40c3b463132c9cbaeebc93bb
ilim0507 <ilim0507@gmail.com>
2023-11-15 10:38:51 +0900

    Format Demo Commit

--- DemoFormat.py
+++ DemoFormat.py
@@ -15,3 +15,37 @@ for x in range(1,6):
 print("---0으로 채우기---")
 for x in range(1,6):
     print(x,"*",x,"=",str(x*x).zfill(5))
+
+#파일 쓰기
+f = open(r"c:\\work\\test.txt","wt",encoding="utf-8") #"wt" always overwrites the previous file
+f.write("first line\nsecond line\nthird line\n")
+f.close()
+
+#파일에 첨부
+f = open(r"c:\\work\\test.txt","a+",encoding="utf-8") #"a+" appends on the existing file
+f.write("additional line\n")
+f.close()
+
+#파일 읽기
+f = open(r"c:\\work\\test.txt","rt",encoding="utf-8")
+print(f.read())
+#f.close()
+
+f.seek(0)#다시 처음으로 리셋
+result = f.readlines()
+for item in result:
+    print(item, end="")
+
+f.seek(0)#다시 처음
+print(f.readline(), end="")
+print(f.readline(), end="")
+
+f.close()
+
+#서식 지정
+print("{0:x}".format(10))
+print("{0:b}".format(10))
+print("{0:,}".format(15000))
+print("{0:e}".format(4/3))
+print("{0:f}".format(4/3))
+print("{0:.2f}".format(4/3))
